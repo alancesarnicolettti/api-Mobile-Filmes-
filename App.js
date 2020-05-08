8022eb16b9befe4d5775c9bc2f369b6b9c16c2f0
Alan Cesar <alancesarnicoletti1998@hotmail.com>
2020-05-08 00:59:05 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+import HomeScreen from './HomeScreen';
+import DetailScreen from './DetailScreen';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content"></StatusBar>
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Netflix" component={HomeScreen} />
+          <Stack.Screen
+            options={({ route }) => ({ title: route.params.movie.title })}
+            name="Detail"
+            component={DetailScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
